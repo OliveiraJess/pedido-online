f11613c850f6f9ca086a4c5b55e069ca8715b4e3
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-12-27 11:04:52 -0300

    começando a impelmentar o menu sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,21 @@
-import { Container } from "./styles"
+import { Container } from "./styles";
+import { ReactComponent as BurguerIcon } from "../../assets/burger.svg";
+import menuImg from "../../assets/menu.svg";
 
 export function Sidebar() {
-  return <Container> Sidebar </Container>
+  return <Container>
+    <button type="button">
+      <img src={menuImg} alt="abrir e fechar menu"/>
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">
+            <BurguerIcon/>
+            <span>Hambúrgueres</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+     </Container>
 }
