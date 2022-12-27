d388f2421f2aaca8cb0e8e90c97cb6ce530f20e4
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-12-27 11:31:50 -0300

    style menu sidebar - button, nav, ul, li, a,  span,

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,11 +1,79 @@
 import styled from "styled-components";
 
 export const Container = styled.aside`
-align-items: center;
-background-color: ${({ theme }) => theme.colors.red};
-display: flex;
-flex-direction: column;
-overflow: hidden;
-padding: 2rem 0;
-width: 7.75rem;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.red};
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  padding: 2rem 0;
+  width: 7.75rem;
+
+  button {
+    background: none;
+    border: none;
+    width: 100%;
+  }
+
+  nav {
+    flex: 1;
+    height: 100%;
+    width: 100%;
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+      height: 100%;
+      justify-content: center;
+    }
+
+    li {
+      a {
+        align-items: center;
+        display: flex;
+        gap: 2rem;
+        padding-left: 1.875rem;
+        padding-right: 1.875rem;
+        position: relative;
+        width: fit-content;
+
+        svg {
+          fill: ${({ theme }) => theme.colors.white};
+          height: 4rem;
+          transition: fill 0.3s;
+          width: 4rem;
+        }
+
+        span {
+          font-size: 1rem;
+          font-weight: 500;
+          transition: color 0.3s;
+        }
+
+        &.active {
+          &::after {
+            background-color: ${({ theme }) => theme.colors.yellow};
+            border-radius: 0 5px 5px 0;
+            bottom: 0;
+            content: '';
+            height: calc(100% + 10px);
+            left: 0;
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            width: 5px;
+          }
+          
+          svg {
+            fill: ${({ theme }) => theme.colors.yellow};
+          }
+
+          span {
+            color: ${({ theme }) => theme.colors.yellow};
+          }
+        }
+      }
+    }
+  }
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,12 +1,46 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  html, body, #root {
-    min-height: 100%;
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
   }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
-    background: ${(props) => props.theme.colors.gray900};
-    color: ${(props) => props.theme.colors.white};
-    font-family: ${(props) => props.theme.fontFamily.sans};
+    background: ${({ theme }) => theme.colors.gray900};
+    -webkit-font-smoothing: antialiased;
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  body,
+  input,
+  select,
+  textarea,
+  button {
+    font: 400 1rem 'Roboto', Helvetica, Arial, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  ul,
+  li {
+    list-style: none;
   }
 `
