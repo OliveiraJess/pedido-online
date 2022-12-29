38a72016538c1eac3ef3a3eea0dbc9cd0ead76db
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-12-29 09:33:26 -0300

    add demais icones

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,21 +1,42 @@
 import { Container } from "./styles";
-import { ReactComponent as BurguerIcon } from "../../assets/burger.svg";
+import { ReactComponent as BurgerIcon } from "../../assets/burger.svg";
+import { ReactComponent as PizzaIcon } from "../../assets/pizza.svg";
+import { ReactComponent as SodaPopIcon } from "../../assets/soda.svg";
+import { ReactComponent as IceCreamIcon } from "../../assets/ice-cream.svg";
 import menuImg from "../../assets/menu.svg";
 
 export function Sidebar() {
   return <Container>
     <button type="button">
-      <img src={menuImg} alt="abrir e fechar menu"/>
+      <img src={menuImg} alt="abrir e fechar menu" />
     </button>
     <nav>
       <ul>
         <li>
-          <a href="#">
-            <BurguerIcon/>
+          <a href="#" className="active">
+            <BurgerIcon />
             <span>Hambúrgueres</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <PizzaIcon />
+            <span>Pizzas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <SodaPopIcon />
+            <span>Bedidas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <IceCreamIcon />
+            <span>Sorvetes</span>
           </a>
         </li>
       </ul>
     </nav>
-     </Container>
+  </Container>
 }
